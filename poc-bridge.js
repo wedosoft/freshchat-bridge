677c508200f751f850f1682422a7f5cfd17661a4
alan <alan@wedosoft.net>
2025-10-25 18:22:08 +0900

    feat: Enhance CORS handling to support dynamic request headers

--- poc-bridge.js
+++ poc-bridge.js
@@ -699,7 +699,13 @@ app.use('/files', express.static(UPLOADS_DIR));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    const requestedHeaders = req.headers['access-control-request-headers'];
+    if (requestedHeaders) {
+        res.header('Access-Control-Allow-Headers', requestedHeaders);
+    } else {
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
 
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
